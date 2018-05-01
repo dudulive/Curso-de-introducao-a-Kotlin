9dc5c27c2733d7f59735d9a1029fc70a1cf9828e
dudulive <edurodriguesbraz@hotmail.com>
2018-04-30 22:31:05 -0300

    Variáveis

---
+++ src/basics/variables.kt
@@ -0,0 +1,13 @@
+package basics
+
+fun main(args: Array<String>) {
+
+    // É o mesmo que uma variavel no java
+    var name = "Edu"
+    var age: Int
+    // É o mesmo que uma variavel final no java
+    val lastName: String
+    lastName = "Braz"
+
+    print(lastName)
+}
